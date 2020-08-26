d07be2dfb66c0d88fff3c4e0bf0935958927363c
Huynh Chinh <hchinh.17ck1@gmail.com>
2020-08-27 06:08:36 +0700

    add db.js

--- services/db.js
+++ services/db.js
@@ -1,6 +1,1 @@
-const Sequelize = require('sequelize');
-
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:binh0968101798@localhost:5432/doanweb'
-const db = new Sequelize(connectionString);
-
-module.exports = db;
+cghvbjnkm
